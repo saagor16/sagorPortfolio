a6df42ff1e7f08eec7d461062af21adcb0e1706d
saagor16 <saagor.16@gmail.com>
2024-07-01 22:34:09 +0600

    skill change and progressbar change

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,5 @@
-import ProgressBar from '@ramonak/react-progress-bar';
 import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGithub } from 'react-icons/fa';
-import { SiNextdotjs, SiTailwindcss } from 'react-icons/si';
+import { SiTailwindcss } from 'react-icons/si';
 
 const Experience = () => {
   const techs = [
@@ -9,63 +8,43 @@ const Experience = () => {
       icon: <FaHtml5 className="w-16 h-16 mx-auto text-orange-500" />,
       title: "HTML",
       style: "shadow-orange-500",
-      color: "#F16529",
-      labelColor: "#FFFFFF",
-      completed: 90,
+
     },
     {
       id: 2,
       icon: <FaCss3Alt className="w-16 h-16 mx-auto text-blue-500" />,
       title: "CSS",
       style: "shadow-blue-500",
-      color: "#2965F1",
-      labelColor: "#FFFFFF",
-      completed: 85,
+ 
     },
     {
       id: 3,
       icon: <FaJs className="w-16 h-16 mx-auto text-yellow-500" />,
       title: "JavaScript",
       style: "shadow-yellow-500",
-      color: "#F0DB4F",
-      labelColor: "#000000",
-      completed: 80,
+
     },
     {
       id: 4,
       icon: <FaReact className="w-16 h-16 mx-auto text-blue-600" />,
       title: "React",
       style: "shadow-blue-600",
-      color: "#61DAFB",
-      labelColor: "#000000",
-      completed: 75,
+
     },
     {
       id: 5,
       icon: <SiTailwindcss className="w-16 h-16 mx-auto text-sky-400" />,
       title: "Tailwind",
       style: "shadow-sky-400",
-      color: "#38BDF8",
-      labelColor: "#000000",
-      completed: 80,
+  
     },
-    {
-      id: 6,
-      icon: <SiNextdotjs className="w-16 h-16 mx-auto text-white" />,
-      title: "Next JS",
-      style: "shadow-white",
-      color: "#FFFFFF",
-      labelColor: "#000000",
-      completed: 65,
-    },
+
     {
       id: 7,
       icon: <FaGithub className="w-16 h-16 mx-auto text-gray-400" />,
       title: "GitHub",
       style: "shadow-gray-400",
-      color: "#181717",
-      labelColor: "#FFFFFF",
-      completed: 60,
+ 
     },
   ];
 
@@ -83,22 +62,14 @@ const Experience = () => {
         </div>
 
         <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
-          {techs.map(({ id, icon, title, style, color, labelColor, completed }) => (
+          {techs.map(({ id, icon, title, style}) => (
             <div
               key={id}
               className={`shadow-md hover:scale-105 duration-500 py-4 rounded-lg ${style}`}
             >
               {icon}
               <p className="mt-4 font-medium">{title}</p>
-              <div className="px-4 mt-2">
-                <ProgressBar 
-                  completed={completed}
-                  bgColor={color}
-                  baseBgColor="#374151"
-                  labelColor={labelColor}
-                  height="14px"
-                />
-              </div>
+              
             </div>
           ))}
         </div>
