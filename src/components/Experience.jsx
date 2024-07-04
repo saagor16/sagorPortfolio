4c9f88bb5e3af6cdb0e890f0d2f76ba464eba9d4
saagor16 <saagor.16@gmail.com>
2024-07-04 10:17:53 +0600

    Skills

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,6 +1,6 @@
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGithub } from 'react-icons/fa';
-import { SiTailwindcss } from 'react-icons/si';
-import { DiMongodb, DiNodejs, DiFirebase, DiBootstrap } from 'react-icons/di';
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGithub } from "react-icons/fa";
+import { SiTailwindcss } from "react-icons/si";
+import { DiMongodb, DiNodejs, DiFirebase, DiBootstrap } from "react-icons/di";
 
 const Experience = () => {
   const techs = [
@@ -67,12 +67,13 @@ const Experience = () => {
   ];
 
   return (
-    <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full min-h-screen">
+    <div
+      name="experience"
+      className="bg-gradient-to-b from-gray-800 to-black w-full min-h-screen"
+    >
       <div className="lg:pt-32 pt-10  max-w-screen-lg mx-auto p-4 flex flex-col justify-center h-full text-white">
         <div className="text-center mb-8">
-          <p className="text-4xl font-bold p-2 ">
-            Experience
-          </p>
+          <p className="text-4xl font-bold p-2 ">Skills</p>
           <p className="py-6">These are the technologies I have worked with</p>
         </div>
 
@@ -80,10 +81,14 @@ const Experience = () => {
           {techs.map(({ id, icon, title, style }) => (
             <div
               key={id}
-              className={`shadow-md hover:scale-105 duration-500 py-2 md:py-4 rounded-lg ${style} ${id > 5 ? 'sm:mt-10' : ''}`}
+              className={`shadow-md hover:scale-105 duration-500 py-2 md:py-4 rounded-lg ${style} ${
+                id > 5 ? "sm:mt-10" : ""
+              }`}
             >
               {icon}
-              <p className="mt-2 md:mt-4 font-medium text-sm sm:text-base">{title}</p>
+              <p className="mt-2 md:mt-4 font-medium text-sm sm:text-base">
+                {title}
+              </p>
             </div>
           ))}
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     },
     {
       id: 4,
-      link: "experience",
+      link: "skills",
     },
     {
       id: 5,
