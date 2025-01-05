4f2899a7f36f9e80e09b04f4bd7c92986d578363
saagor16 <saagor.16@gmail.com>
2025-01-05 12:31:17 +0600

    all add fdhgj

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -16,22 +16,19 @@ const Portfolio = () => {
     {
       id: 1,
       src: bookStoreApp,
-      name: 'Book Store App',
-      description: 'A dynamic application designed for managing books, featuring user and admin dashboards, graphical stats, and a secure authentication system using Firebase. Built with modern technologies for a seamless and interactive user experience.',
-      githubLink: 'https://github.com/saagor16/Book-Store-Frontend',
-      liveLink: 'https://book-store-app-ff563.web.app',
-      techs: ["React","Redux Toolkit","Firebase","Tailwind CSS","React Router DOM","React Hook Form","Chart.js","Axios","SweetAlert2"
-      ],
+      name: "Book Store App",
+      description:
+        "A dynamic application designed for managing books, featuring user and admin dashboards, graphical stats, and a secure authentication system using Firebase. Built with React, Redux Toolkit, and Tailwind CSS for a responsive and interactive user experience.",
+      githubLink: "https://github.com/saagor16/Book-Store-Frontend",
+      liveLink: "https://book-store-app-ff563.web.app/",
+      techs: ["React", "Redux Toolkit", "Firebase", "Tailwind CSS"],
       features: [
-        'User Dashboard for Orders and Cart', 
-        'Admin Panel for Book Management', 
-        'Graphical Stats Visualization', 
-        'Responsive Design',
-        'Secure Authentication',
-        'Order Management',
-        'Book Addition and Deletion'
+        "User Dashboard for Orders and Cart",
+        "Admin Panel for Book Management",
+        "Graphical Stats Visualization",
+        "Responsive Design",
       ],
-    },    
+    },  
     {
       id: 2,
       src: campusHub,
